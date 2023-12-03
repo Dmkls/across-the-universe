d42c3155e5b56b748be9daa1bd6d981103e140e3
dmitry <kovtunov.da@students.dvfu.ru>
2023-12-03 11:50:16 +1000

    Update field generation function

--- extra/GenerateFieldPoints.ts
+++ extra/GenerateFieldPoints.ts
@@ -1,40 +1,39 @@
+import Spline from 'typescript-cubic-spline';
+
 /*
-GenerateFieldPoints - функция которая возвращает массив точке (x, y) для создания игрового поля
+GenerateGameField - функция генерации точек для отрисовки игрового поля
+
+Формат вызова:
+    GenerateGameField(xStart: number, xStep: number, pointNumber: number, splineStep: number = 5)
 
 Параметры:
-    start_x - стартовое значение Х
-    finish_x - конечное значение Х
-    step_x - шаг для задания точек по Х
-    min_y - минимальнодопутимое значение по У
-    max_y - максимальнодопустимое значение по У
-    height_coef - коэфициент генерации высот(чем он выше, тем больше разброс высот)
-    spline_coef - коэфициент фильтрации значений. Допустимые значения от 0-1(Чем меньше значение,
-    тем плавнее переход)
+    xStart - стартовое значение x
+    xStep - шаг для генерации новой высоты
+    pointNumber - количество точек по х
+    splineStep - шаг сглашивания(чем больше шаг, тем меньше сглаживание)
+
+Возвращает массив из точек в формате (x, y)
  */
 
-
-function GenerateFieldPoints(start_x: number, finish_x: number, step_x: number = 1, min_y: number = -500,
-                             max_y: number = 500, height_coef: number = 10, spline_coef: number = 0.05) {
-    let x_arr: number[] = [];
-    let y_arr: number[] = [];
-    let temp: number = 0;
-
-    for (let x = start_x; x <= finish_x; x += step_x) {
-        x_arr.push(x);
-        let y: number = Math.floor((Math.random() - .5) * height_coef);
-
-        if (y > max_y || y < min_y) {
-            y *= .5;
-        }
-
-        temp += (y - temp) * spline_coef
-        y_arr.push(y);
+function GenerateGameField(xStart: number, xStep: number, pointNumber: number, splineStep: number = 5) {
+    function randomInt(min: number, max: number) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+
+    let xArr: number[] = [];
+    let yArr: number[] = [];
+
+    for (let i = 0; i < pointNumber; i++) {
+        xArr.push(xStart + xStep * i);
+        yArr.push(randomInt(-100, 100));
+    }
+
+    let spline = new Spline(xArr, yArr);
 
     let points: number[][] = [];
-    for (let i = 0; i < x_arr.length; i++) {
-        points.push([x_arr[i], y_arr[i]])
+    for (let x = xStart; x <= xStep * (pointNumber - 1); x += splineStep) {
+        points.push([x, spline.at(x)]);
     }
 
-    return points
+    return points;
 }
